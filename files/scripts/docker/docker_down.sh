6be5708ce51e8d105e7b749debcca7ebb8ffc6a2
loupnunux <loupnunux@gmail.com>
2020-03-16 10:01:04 +0100

    add up and down

---
+++ files/scripts/docker/docker_down.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cd /opt/docker/docker_compose/
+sudo docker-compose down
+sudo docker-compose rm -f
+
---
+++ files/scripts/docker/docker_up.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd /opt/docker/docker_compose/
+sudo docker-compose up -d
+
